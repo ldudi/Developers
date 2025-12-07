ccb1b85e1b47a138fa4275e58e3f46274ef5e931
Labhesh <labhesh.dudi@gmail.com>
2025-12-07 11:23:35 +0530

    git initial project setup

---
+++ Developers/Developers/DevelopersApp.swift
@@ -0,0 +1,17 @@
+//
+//  DevelopersApp.swift
+//  Developers
+//
+//  Created by Labhesh Dudi on 07/12/25.
+//
+
+import SwiftUI
+
+@main
+struct DevelopersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
