2f0cc4d2d1f7c1688b82c73aed5daf18028bff37
Labhesh <labhesh.dudi@gmail.com>
2025-12-15 12:15:28 +0530

    animation

--- Developers/Developers/CardView.swift
+++ Developers/Developers/CardView.swift
@@ -12,6 +12,10 @@ struct CardView: View {
     
     var card: Card
     
+    @State private var fadeIn: Bool = false
+    @State private var moveDownward: Bool = false
+    @State private var moveUpward: Bool = false
+    
     var gradient: [Color] = [Color("Color01"), Color("Color02")]
     
     
@@ -20,6 +24,7 @@ struct CardView: View {
     var body: some View {
         ZStack {
             Image(card.imageName)
+                .opacity(fadeIn ? 1 : 0)
             
             VStack {
                 Text(card.title)
@@ -32,7 +37,7 @@ struct CardView: View {
                     .foregroundColor(Color.white)
                     .italic()
             }
-            .offset(y: -218)
+            .offset(y: moveDownward ? -218: -300)
             
             Button(action: {
                 playSound(fileName: "sound-chime", type: "mp3")
@@ -53,15 +58,24 @@ struct CardView: View {
                 .clipShape(Capsule())
                 .shadow(color: Color("ColorShadow"), radius: 6, x: 0, y: 3)
             }
-            .offset(y: 210)
+            .offset(y: moveUpward ? 210: 300)
         }
         .frame(width: 355, height: 545)
         .background(LinearGradient(gradient: Gradient(colors: card.gradientColors), startPoint: .top, endPoint: .bottom))
         .cornerRadius(16)
         .shadow(radius: 8)
+        .onAppear {
+            withAnimation(.linear(duration: 1.2)) {
+                self.fadeIn.toggle()
+            }
+            withAnimation(.linear(duration: 0.8)) {
+                self.moveDownward.toggle()
+                self.moveUpward.toggle()
+            }
+        }
     }
 }
 
 #Preview(traits: .sizeThatFitsLayout) {
-    CardView(card: cardData[0])
+    CardView(card: cardData[1])
 }
